01b38483d5f66cb30373ac971ab6c28627588578
csemler26 <csemler26@gmail.com>
2024-08-25 13:36:09 -0500

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <iomanip>
 #include <filesystem>
-#include <fstream>
 #include <curl/curl.h>
 #include "include/json.h"
 #include "include/market.h"
@@ -10,51 +9,15 @@
 #include "include/dataTypes.h"
 #include "include/watchlist.h"
 #include "include/database.h"
+#include "include/parameterManager.h"
 
 using namespace std;
 
-// Helper function to trim spaces and quotes
-std::string trim(const std::string& str) {
-  size_t first = str.find_first_not_of(' ');
-  size_t last = str.find_last_not_of(' ');
-  return str.substr(first, (last - first + 1));
-}
-
-// Function to load environment variables from a .env file
-void load_env_file(const std::string& filepath) {
-  std::ifstream file(filepath);
-  if (!file.is_open()) {
-    std::cerr << "Could not open .env file" << std::endl;
-    return;
-  }
-
-  std::string line;
-  while (std::getline(file, line)) {
-    // Ignore empty lines and comments
-    if (line.empty() || line[0] == '#') continue;
-
-    size_t delimiter_pos = line.find('=');
-    if (delimiter_pos == std::string::npos) continue;
-
-    std::string key = trim(line.substr(0, delimiter_pos));
-    std::string value = trim(line.substr(delimiter_pos + 1));
-
-    // Remove any quotes around the value
-    if (value.front() == '"' && value.back() == '"') {
-        value = value.substr(1, value.size() - 2);
-    }
-
-    // Set the environment variable (on POSIX systems)
-    setenv(key.c_str(), value.c_str(), 1);
-  }
-  file.close();
-}
-
-void init(Portfolio& portfolio)
+void init(Portfolio& portfolio, ParameterManager& paramManager)
 {
   portfolio.loadPortfolio();
-  // portfolio.printPortfolio();
-  load_env_file(".env");
+  portfolio.printPortfolio();
+  paramManager.load_env_file();
 }
 
 int main()
@@ -62,9 +25,10 @@ int main()
   string input;
   Portfolio portfolio;
   Watchlist watchlist;
+  ParameterManager parameterManager;
   Database db("financial_portfolio.db");
   
-  init(portfolio);
+  init(portfolio, parameterManager);
 
   while (1)
   {
